85623faf99c6f3d6e80af94f16afb0000b116ba1
Daniel <danielmorernoinf@gmail.com>
2024-03-15 19:19:45 -0300

    se arregla identacion

--- imc.py
+++ imc.py
@@ -8,22 +8,15 @@ talla_altura = talla_altura/100
 imc = peso_kg / (talla_altura**2)
 print(f'su IMC es de: ', round(imc,2))
 
-if peso_kg >= 1:
-    if (imc < 18.5):
-        print('La clasificación OMS es Bajo Peso')
-    elif (imc >= 18.5 and imc < 25):
-        print('La clasificación OMS es Adecuado')
-    elif (imc >= 25 and imc < 30):
-        print('La clasificación OMS es Sobrepeso')
-    elif (imc >= 30 and imc < 35):
-        print('La clasificación OMS es Obesidad Grado I')
-    elif (imc >= 35 and imc < 40):
-        print('La clasificación OMS es Obesidad Grado II')
-    else:
-        print('La clasificación OMS es Obesidad Grado III')
+if (imc > 0 and imc < 18.5):
+    print('La clasificación OMS es Bajo Peso')
+elif (imc >= 18.5 and imc < 25):
+    print('La clasificación OMS es Adecuado')
+elif (imc >= 25 and imc < 30):
+    print('La clasificación OMS es Sobrepeso')
+elif (imc >= 30 and imc < 35):
+    print('La clasificación OMS es Obesidad Grado I')
+elif (imc >= 35 and imc < 40):
+    print('La clasificación OMS es Obesidad Grado II')
 else:
-    print('ERROR - Peso no puede ser 0')
-    
-    
-
-
+    print('La clasificación OMS es Obesidad Grado III')
